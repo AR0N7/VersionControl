29ca11dc2252be4925b00509eac66e3e4d02ef0c
Áron Tompai <aron.tompai@stud.uni-corvinus.hu>
2020-10-21 18:16:18 +0200

    7.

--- Microsimulation_GQOTXA/Microsimulation_GQOTXA/Form1.cs
+++ Microsimulation_GQOTXA/Microsimulation_GQOTXA/Form1.cs
@@ -31,7 +31,7 @@ namespace Microsimulation_GQOTXA
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    //szim lépés
+                    SimStep();
                 }
                 int nbrOfMales = (from x in Population
                                   where x.Gender == Gender.Male && x.IsAlive
@@ -41,6 +41,36 @@ namespace Microsimulation_GQOTXA
                                     select x).Count();
                 Console.WriteLine(
                     string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender==Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
             }
         }
 
