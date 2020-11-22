948da7d6c87b2854fdaaaa3c47037347b8c60dde
Áron Tompai <aron.tompai@stud.uni-corvinus.hu>
2020-11-22 13:31:14 +0100

    7-8.

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -27,5 +27,24 @@ namespace UnitTestExample.Test
 
            
         }
+
+        [
+            Test,
+            TestCase("abcddafsSAEJHJK", false),
+            TestCase("ABCD92898", false),
+            TestCase("asasdsds98317", false),
+            TestCase("a1A", false),
+            TestCase("aAbCdEF2113Ae", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
     }
 }
